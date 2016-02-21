6f37252b27a0273d955caed9b15cbebd945b9306
Andrea Schweer <schweer@waikato.ac.nz>
2016-02-22 11:28:40 +1300

    |#8130 First stab at surfacing top authors / collection

---
+++ src/main/java/nz/ac/waikato/its/dspace/app/xmlui/aspect/surfacecontent/SurfaceContentDataProvider.java
@@ -0,0 +1,9 @@
+package nz.ac.waikato.its.dspace.app.xmlui.aspect.surfacecontent;
+
+/**
+ * @author Andrea Schweer schweer@waikato.ac.nz
+ *         for the University of Waikato's Institutional Research Repositories
+ */
+public @interface SurfaceContentDataProvider {
+    String key();
+}
